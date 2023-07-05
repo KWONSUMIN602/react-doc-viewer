41233fab6b21d1653536182b4c6a554aa7efb469
kwonsumin602 <92791424+KWONSUMIN602@users.noreply.github.com>
2023-07-05 18:15:32 +0900

    Update input file upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+// import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const docs = [
+    { uri: require("./sample.pdf") },
+    { uri: require("./sample3.txt") },
+    { uri: require("./file-sample.doc") },
+    { uri: require("./test.docx") },
+    { uri: require("./sample3.xlsx") },
+  ];
+
+  // const [selectedDocs, setSelectedDocs] = useState([]);
+
+  const MyNoRenderer = ({ document, fileName }) => {
+    const fileText = fileName || document?.fileType || "";
+
+    if (fileText) {
+      return <div>No Renderer Error! ({fileText})</div>;
+    }
+
+    return <div>No Renderer Error!</div>;
+  };
+
+  const MyLoadingRenderer = ({ document, fileName }) => {
+    const fileText = fileName || document?.fileType || "";
+
+    if (fileText) {
+      return <div>Loading Renderer ({fileText})...</div>;
+    }
+
+    return <div>Loading Renderer...</div>;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>viewer-test</h1>
+
+      <>
+        {/* <input
+          type="file"
+          accept=".pdf,.xlsx,.doc,.docx,.txt"
+          multiple
+          onChange={(el) =>
+            el.target.files?.length &&
+            setSelectedDocs(Array.from(el.target.files))
+          }
+        /> */}
+
+        <DocViewer
+          pluginRenderers={DocViewerRenderers}
+          documents={docs}
+          // documents={selectedDocs.map((file) => ({
+          //   uri: window.URL.createObjectURL(file),
+          //   fileName: file.name,
+          // }))}
+          config={{
+            loadingRenderer: {
+              overrideComponent: MyLoadingRenderer,
+              showLoadingTimeout: false, // false if you want to disable or number to provide your own value (ms)
+            },
+            noRenderer: {
+              overrideComponent: MyNoRenderer,
+            },
+          }}
+        />
+      </>
     </div>
   );
 }
-
-export default App;
